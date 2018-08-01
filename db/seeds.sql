3304ae68273b9d6f18fa288380fbbeaaabc7042e
Daemone <kylebehm@gmail.com>
2018-07-31 22:01:31 -0400

    schema

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheesburger');
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
+INSERT INTO burgers (burger_name) VALUES ('Southwest Burger');
