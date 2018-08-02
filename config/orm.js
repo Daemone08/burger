fcca267af3d62cba5808e0e08997cd2d65b250d5
Daemone <kylebehm@gmail.com>
2018-08-01 22:41:07 -0400

    orm-pseudo

--- config/orm.js
+++ config/orm.js
@@ -3,11 +3,52 @@ var connection = require("../config/connection.js")
 // ORM object for all SQL queries
 var orm = {
 
-    selectAll: func,
+    // selectAll method takes two arguments
+    selectAll: function (tableTarget, callback) {
+        // first the query string must be created in the form SELECT * FROM table;
+        var queryString = "SELECT * FROM " + tableTarget + ";";
+        // next the sql query is run using the sql query syntax
+        connection.query(queryString, function (err, result) {
+            // check for errors + throw
+            if (err) {
+                throw err;
+            }
+            // otherwise pass the result to the callback
+            callback(result);
+        })
+    },
 
-    insertOne: func,
+    // insertOne method takes four arguments (columns and values are arrays)
+    insertOne: function (tableTarget, columns, values, callback) {
+        // first the query string must be created in the form INSERT INTO table (columns) VALUES (values);
+        // *****columns and values must be strings********values is values.length number of ?,? for escape purposes
+        queryString = "INSERT INTO " + tableTarget + " (" + columns + ") (" + values + ");"
+        // next the sql query is run using the sql query syntax (values is still the same array)
+        connection.query(queryString, values, function (err, result) {
+            // check for errors + throw
+            if (err) {
+                throw err;
+            }
+            // otherwise pass the result to the callback
+            callback(result);
+        })
+    },
 
-    updateOne: func
+    // updateOne method takes four arguments
+    updateOne: function (tableTarget, columnValuesObj, condition, callback) {
+        // first the query string must be created in the form UPDATE table SET col1name=value1,col2name=value2,etc WHERE condition
+        // ***columnValuesObj must be converted to string***
+        var queryString = "UPDATE " + tableTarget + " SET " + columnValuesObj + " WHERE " + condition + ";";
+        // next the sql query is run using the sql query syntax
+        connection.query(queryString, function (err, result) {
+            // check for errors + throw
+            if (err) {
+                throw err;
+            }
+            // otherwise pass the result to the callback
+            callback(result);
+        })
+    }
 
 }
 
