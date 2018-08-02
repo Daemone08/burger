935eec126decb4a29df3dccda96f8753ad664f49
Daemone <kylebehm@gmail.com>
2018-08-01 22:49:28 -0400

    orm-with-format-helpers

--- config/orm.js
+++ config/orm.js
@@ -1,4 +1,52 @@
+// Require the mysql connection
 var connection = require("../config/connection.js")
+
+
+// USING HELPER FUNCTIONS FROM CatsApp TO FORMAT QUERYSTRING ARGUMENTS FOR SQL SYNTAX AND ESCAPING
+// -----------------------------------------------------------------------------------------------
+
+// Helper function for SQL syntax.
+// Let's say we want to pass 3 values into the mySQL query.
+// In order to write the query, we need 3 question marks.
+// The above helper function loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string.
+// ["?", "?", "?"].toString() => "?,?,?";
+function printQuestionMarks(num) {
+    var arr = [];
+
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+
+    return arr.toString();
+}
+
+// Helper function to convert object key/value pairs to SQL syntax
+function objToSql(ob) {
+    var arr = [];
+
+    // loop through the keys and push the key/value as a string int arr
+    for (var key in ob) {
+        var value = ob[key];
+        // check to skip hidden properties
+        if (Object.hasOwnProperty.call(ob, key)) {
+            // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+            // e.g. {sleepy: true} => ["sleepy=true"]
+            arr.push(key + "=" + value);
+        }
+    }
+
+    // translate array of strings to a single comma-separated string
+    return arr.toString();
+}
+
+
+// END OF HELPER FUNCTIONS FROM CatsApp 
+// ------------------------------------
+
 
 // ORM object for all SQL queries
 var orm = {
@@ -22,7 +70,7 @@ var orm = {
     insertOne: function (tableTarget, columns, values, callback) {
         // first the query string must be created in the form INSERT INTO table (columns) VALUES (values);
         // *****columns and values must be strings********values is values.length number of ?,? for escape purposes
-        queryString = "INSERT INTO " + tableTarget + " (" + columns + ") (" + values + ");"
+        queryString = "INSERT INTO " + tableTarget + " (" + columns.toString() + ") (" + printQuestionMarks(values.length) + ");"
         // next the sql query is run using the sql query syntax (values is still the same array)
         connection.query(queryString, values, function (err, result) {
             // check for errors + throw
@@ -38,7 +86,7 @@ var orm = {
     updateOne: function (tableTarget, columnValuesObj, condition, callback) {
         // first the query string must be created in the form UPDATE table SET col1name=value1,col2name=value2,etc WHERE condition
         // ***columnValuesObj must be converted to string***
-        var queryString = "UPDATE " + tableTarget + " SET " + columnValuesObj + " WHERE " + condition + ";";
+        var queryString = "UPDATE " + tableTarget + " SET " + objToSql(columnValuesObj) + " WHERE " + condition + ";";
         // next the sql query is run using the sql query syntax
         connection.query(queryString, function (err, result) {
             // check for errors + throw
