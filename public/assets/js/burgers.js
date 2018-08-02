333146e8dad3fcf138769d792ea20dea8801fcf4
Daemone <kylebehm@gmail.com>
2018-08-02 18:02:07 -0400

    functional-submission

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,24 +1,24 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
-    // $(".devour").on("click", function(event) {
-    //   var id = $(this).data("id");
+    $(".devour").on("click", function(event) {
+      var id = $(this).data("id");
   
-    //   var newDevourState = {
-    //     devoured: true
-    //   };
+      var newDevourState = {
+        devoured: true
+      };
   
-    //   // Send the PUT request.
-    //   $.ajax("/api/burgers/" + id, {
-    //     type: "PUT",
-    //     data: newDevourState
-    //   }).then(
-    //     function() {
-    //       console.log("Burger " + id + " has been devoured and updated");
-    //       // Reload the page to get the updated list
-    //       location.reload();
-    //     }
-    //   );
-    // });
+      // Send the PUT request.
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: newDevourState
+      }).then(
+        function() {
+          console.log("Burger " + id + " has been devoured and updated");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
   
     $(".create-form").on("submit", function(event) {
       // Make sure to preventDefault on a submit event.
