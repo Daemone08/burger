b1ba8941dae36b1796de80e22293b28784dd5318
Daemone <kylebehm@gmail.com>
2018-08-01 21:52:53 -0400

    skeleton

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,13 @@
+// Require express
+var express = require("express");
+// Require router
+var router = express.router();
+// Require model
+var burger = require("../models/burger.js");
+
+// Create all our routes and set up logic within those routes where required.
+
+
+
+// Export routes for server.js to use.
+module.exports = router;
