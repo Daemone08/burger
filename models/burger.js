707467ee23fe4097392a9ae888bf2a37dcb97b47
Daemone <kylebehm@gmail.com>
2018-08-01 21:40:23 -0400

    basic-model

---
+++ models/burger.js
@@ -0,0 +1,24 @@
+// Import the ORM
+var orm = require("../config/orm.js");
+
+var burger = {
+    selectAll: function(callback) {
+      orm.selectAll("burgers", function(res) {
+        callback(res);
+      });
+    },
+    // The variables cols and vals are arrays.
+    insertOne: function(cols, vals, callback) {
+      orm.insertOne("burgers", cols, vals, function(res) {
+        callback(res);
+      });
+    },
+    updateOne: function(objColVals, condition, callback) {
+      orm.updateOne("burgers", objColVals, condition, function(res) {
+        callback(res);
+      });
+    }
+  };
+
+// Export the database functions for the controller
+module.exports = burger;
